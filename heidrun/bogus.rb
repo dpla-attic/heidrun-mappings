fdf2160982325f003110a6b8c7abc64ee6db81d8
Mark A. Matienzo <mark@matienzo.org>
2015-03-11 12:41:41 -0400

    Correct spelling

--- heidrun/bogus.rb
+++ heidrun/bogus.rb
@@ -2,6 +2,6 @@
 Krikri::Mapper.define(:bogus_dc, :parser => Krikri::OaiDcParser) do
   provider :class => DPLA::MAP::Agent do
     uri 'http://dp.la/api/contributor/bogus'
-    label 'Bogus Contributor'
+    label 'The Bogus Contributor'
   end
 end
